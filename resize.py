d6332b28b4872ada6b8bda6c1c250acc9430333d
zerorax <willoiffer@yahoo.com>
2017-02-15 20:24:21 -0500

    fixed cygwin term resize

--- resize.py
+++ resize.py
@@ -6,5 +6,7 @@ def ResizeScreen(x,y):
 		clientos = platform.system()
 		if clientos == 'Windows':
 			os.system("mode {rows},{columns}".format(rows=x,columns=y))
-		elif clientos == 'Linux' or clientos == 'Darwin' or clientos.startswith('CYGWIN'):
+		elif clientos == 'Linux' or clientos == 'Darwin':
 			sys.stdout.write("\x1b[8;{columns};{rows}t".format(rows=x,columns=y))
+		elif clientos.startswith('CYGWIN'):
+			os.system("echo -ne '\e[8;{columns};{rows}t'".format(rows=x,columns=y))
